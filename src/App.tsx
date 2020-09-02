ac4c917acebaaece0bfd610652fcd4ec0063ed7f
Nay Ferreira <ncy.fers@gmail.com>
2020-09-01 22:43:49 -0300

    :pencil2: applying global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BancoInter  from './assets/images/logo.svg';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme'
+import GlobalStyle from './styles/global'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+			<GlobalStyle />
       <h1>Banco Inter</h1>
 			<img src={BancoInter} alt="{BancoInter}" />
-    </div>
+    </ThemeProvider>
 
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-
 		* {
 			margin: 0;
 			padding: 0;
@@ -9,22 +8,69 @@ export default createGlobalStyle`
 			outline: 0;
 		}
 
+		html {
+			font-size: 62.5%; 
+			height: 100%;
+			@media (min-width: 1981px) {
+				font-size: 80%;
+				};
+  	}
+
+		body, #root {
+			min-height: 100vh;
+			height: 100%;
+			position: relative;
+			display: flex;
+			flex-direction: column;
+  	}
+
 		body, input, button {
 			font-family: 'Inter', 'Roboto';
 			font-size: 16px;
-			-webkit-font-smoothing: antialiased;
+			-webkit-font-smoothing: antialiased !important;
+			line-height: 1.48;
 		}
 
-		h1,h2,h3,h4,h5{
-			font-weight: 500
+		body {    
+			background: ${({ theme }) => theme.colors.lightGrey};
+			color: ${({ theme }) => theme.colors.grey};
+	    text-rendering: optimizeLegibility !important;
+  	  -webkit-font-smoothing: antialiased !important;
+   	  -moz-osx-font-smoothing: grayscale;
+   	}
+
+	  a {
+			text-decoration: none;
+			background: none;
+			font-weight: 700;
+			cursor: pointer;
+			border: 0;
+			transition: 180ms ease-in-out;
+		}
+
+		h1, h2, h3, h4, h5 {
+			font-weight: 500;
 		}
 
 		strong {
-			font-weight: 800
+			font-weight: 800;
 		}
 
 		button {
-			cursor: pointer
+			cursor: pointer;
+			border: 0;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 		}
-		
-`
+
+		ul {
+  	  list-style: none;
+   	 	text-align: left;
+   	 	padding: 0;
+  	}
+
+		img {
+			width: 45px;
+		}
+`;
